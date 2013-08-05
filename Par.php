a04a7e1d46c60cd9e503fb116199311b589249f6
zhangkh <zhangkh@zhangkh-jinshan.(none)>
2013-08-05 18:04:43 +0800

    aaaa

--- Par.php
+++ Par.php
@@ -15,17 +15,17 @@ class Par extends CActiveRecord
      * @param array $entry=array($key=>$value) key is the partition field name.
      * @param array $opts some optitions: hash_num=>the hash num default 10.
      */
-    public function __construct($entry = array(), array $opts = null){
-        if($entry === null){
+    public function __construct($opts = array(), $entry = array()){
+        if($opts === null){
             return;
         }
-        $this->parseParams($entry, $opts);
+        $this->parseParams($opts, $entry);
         parent::__construct();
     }
 
-    public static function model($entry = array(), array $opts = null, $className=__CLASS__)
+    public static function model($opts = null, $entry = array(), $className=__CLASS__)
     {
-        $model = new $className($entry, $opts);
+        $model = new $className($opts, $entry);
         return $model;
     }
 
@@ -34,7 +34,7 @@ class Par extends CActiveRecord
         return static::$tablename;
     }
 
-    protected function parseParams($entry, $opts){
+    protected function parseParams($opts, $entry){
         if(isset($opts['hash_num'])){
             $this->hashnum = $opts['hash_num'];
         }
@@ -44,16 +44,16 @@ class Par extends CActiveRecord
         if(!empty($entry)){
             $key = key($entry);
             $value = $entry[$key];
-            $this->_key = $key;
-            static::$tablename .= '_'.$this->getHashTablename($value);
+            $this->_key = $value;
         }
+        static::$tablename .= '_'.$this->getHashTablename();
     }
 
     //根据字符串hash到十进制，计算表hash值
-    protected function getHashTablename($key){
+    protected function getHashTablename(){
         switch($this->hashtype){
         case self::STRING_HASH:
-            $str = base_convert(md5($key),16, 10);
+            $str = base_convert(md5($this->_key),16, 10);
             $key_hash = substr($str, strlen($str)-1, 1);
             $table_hash = $key_hash % $this->hashnum;
             break;
